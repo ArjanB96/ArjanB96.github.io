249a2cc5e96147f9c1a2592efb1257239c403a3a
Arjan <74960799+ArjanB96@users.noreply.github.com>
2023-10-31 00:42:18 +0100

    added loading screen

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,11 @@ $(document).ready(function () {
         $(".hamburger-button").toggleClass("navbar-toggler-icon-active");
     });
 });
+
+// Function to hide the loading overlay after 3 seconds
+window.onload = function() {
+    setTimeout(function() {
+        document.querySelector('.loading-overlay').style.display = 'none';
+    }, 1000); // Hide after 1 second (1000 milliseconds)
+};
+
