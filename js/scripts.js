c48e666ba3610a2785a6e91151ed37602d0db209
Arjan <74960799+ArjanB96@users.noreply.github.com>
2023-10-31 01:15:54 +0100

    iphone fix 4

--- js/scripts.js
+++ js/scripts.js
@@ -10,3 +10,7 @@ window.onload = function() {
         document.querySelector('.loading-overlay').style.display = 'none';
     }, 1000); // Hide after 1 second (1000 milliseconds)
 };
+
+document.body.addEventListener('touchmove', function(event) {
+    event.preventDefault();
+}, { passive: false });
